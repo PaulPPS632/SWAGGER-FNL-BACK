9108de7949a561943b03daa24bfb91a6a75d429f
landeo29 <158001812+landeo29@users.noreply.github.com>
2025-02-27 14:02:53 -0500

    actualizacion prediccion

--- src/services/predictionService.ts
+++ src/services/predictionService.ts
@@ -1,11 +1,14 @@
 import axios from "axios";
 
-export const predictStress = async (): Promise<any> => {
+export const predictStress = async (empresaId: number): Promise<any> => {
     try {
-        const response = await axios.get("http://127.0.0.1:8000/predict");
+        console.log(`solicitando predicción para empresa_id: ${empresaId}`);
+
+        const response = await axios.get(`http://127.0.0.1:8000/predict?id_empresa=${empresaId}`);
         return response.data;
-    } catch (error) {
-        console.error("Error en la predicción:", error);
-        throw new Error("No se pudo obtener la predicción");
+
+    } catch (error: any) {
+        console.error("wrror en la predicción:", error.message || error);
+        throw new Error("no se pudo obtener la predicción");
     }
 };
